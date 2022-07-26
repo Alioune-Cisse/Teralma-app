e7828fb962a6fc1fe0525af7e4ebe496b85c9dbf
Alioune CISSE <60492447+Alioune-Cisse@users.noreply.github.com>
2022-07-26 22:41:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from basics import optimiser_depense, getdata, repartition
+import json
+import ast
 
 donnees = getdata()
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 @app.route('/')
 def index():
@@ -24,6 +27,18 @@ def result():
 
     else:
         return render_template('after.html')
+   
+
+@app.route('/api/v1')
+def my_route():
+  budget = request.args.get('budget', default = 1200000, type = int)
+  services = request.args.get('services', default = ["Traiteur", "Photo"], type = str)
+  services = ast.literal_eval(services)
+  invites = request.args.get('invites', default=100, type=int)
+  print(f'services = {services}\nTypes = {type(services)}')
+  repartitions = repartition(optimiser_depense(int(budget),  services))
+  print(repartitions)
+  return jsonify(repartitions)
 
 
 if __name__ == "__main__":
